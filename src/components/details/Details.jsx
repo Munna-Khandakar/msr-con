6eb1eb67b9f3ee02c0ac104255383368847e4253
Munna-Khandakar <munnashisad@gmail.com>
2024-03-19 23:56:05 +0600

    details page text modified

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -53,7 +53,7 @@ function JobDetail() {
         {/* Job Detail Start */}
           <div className="container">
             <div className="detail-body">
-              <div className="">
+              <div>
                 <div className="marketing-manager">
                   <img className='logo' src='/assets/img-job-entry/com-logo-2.jpg' alt="" style={{ width: '80px', height: '80px' }} />
                   <div className=" text-start ps-4">
@@ -62,47 +62,55 @@ function JobDetail() {
                 </div>
   
                 <div className="description-container">
-                  <h4 className="description-title">Job description</h4>
-                  <p>Dolor justo tempor duo ipsum accusam rebum gubergren erat. Elitr stet dolor vero clita labore gubergren. Kasd sed ipsum elitr clita rebum ut sea diam tempor. Sadipscing nonumy vero labore invidunt dolor sed, eirmod dolore amet aliquyam consetetur lorem, amet elitr clita et sed consetetur dolore accusam. Vero kasd nonumy justo rebum stet. Ipsum amet sed lorem sea magna. Rebum vero dolores dolores elitr vero dolores magna, stet sea sadipscing stet et. Est voluptua et sanctus at sanctus erat vero sed sed, amet duo no diam clita rebum duo, accusam tempor takimata clita stet nonumy rebum est invidunt stet, dolor.</p>
-                  <h4 className="job-responsibility">Responsibility</h4>
-                  <p>Magna et elitr diam sed lorem. Diam diam stet erat no est est. Accusam sed lorem stet voluptua sit sit at stet consetetur, takimata at diam kasd gubergren elitr dolor</p>
+                  <p>Bangladesh is assessed as Level 2 country in Australia which meansless documentation is required to apply. 
+                    Now the visa success rate is very high for Bangladesh. 
+                    If you wish to apply in Australia, please do not waste time
+                  </p>
+                  <h4 className="job-responsibility">PROCESS TO APPLY</h4>
+                  <p>Applying is very easy. You just need to follow the following steps</p>
                   <ul className="list-unstyled">
-                  <li> <FaAngleRight className="react-icon" />lor justo tempor duo ipsum accusam</li>
-                    <li><FaAngleRight className="react-icon" />Elitr stet dolor vero clita labore gubergren</li>
-                    <li><FaAngleRight className="react-icon" />Rebum vero dolores dolores elitr</li>
-                    <li> <FaAngleRight className="react-icon" />Est voluptua et sanctus at sanctus erat</li>
-                    <li> <FaAngleRight className="react-icon" />Diam diam stet erat no est est</li>
-                  </ul>
-                  <h4 className="description-title">Qualifications</h4>
-                  <p>Magna et elitr diam sed lorem. Diam diam stet erat no est est. Accusam sed lorem stet voluptua sit sit at stet consetetur, takimata at diam kasd gubergren elitr dolor</p>
-                  <ul className="list-unstyled">
-                    <li><FaAngleRight className="react-icon" />Dolor justo tempor duo ipsum accusam</li>
-                    <li><FaAngleRight className="react-icon" />Elitr stet dolor vero clita labore gubergren</li>
-                    <li><FaAngleRight className="react-icon" />Rebum vero dolores dolores elitr</li>
-                    <li> <FaAngleRight className="react-icon" />Est voluptua et sanctus at sanctus erat</li>
-                    <li> <FaAngleRight className="react-icon" />Diam diam stet erat no est est</li>
+                  <li> <FaAngleRight className="react-icon" />
+                  STEP 1- Offer Letter (Time Required - 14 days): Choose the study program (You must have a valid IELTS result, required previous study, Explained GAP period if any, Financial Evidence, and a good SOP to meet the GTE requirements )
+</li>
+                    <li><FaAngleRight className="react-icon" />STEP 2 - COE (Time Required - 3-7 days):
+Pay your tuition fees and insurance fees
+(Once you sign the offer, pay your tuition fees to receive the Confirmation of Enrolment)</li>
+                    <li><FaAngleRight className="react-icon" />STEP 3 - Visa Application (Time Required - 1 or 2 days):
+Submit your visa application and book an appointment for Biometric and medical examination
+(Once you receive the COE, apply through the Immigration website. You need to do Biometric and Medical test. Include all evidence accordingly. You need to write a good Statement of Purpose (SOP).</li>
+<li><FaAngleRight className="react-icon" />STEP 4 - Visa Decision (Time Required - 17 to 45 days):
+Usual process time is 17 days - 45 days to get a decision.</li>
+                    <li><FaAngleRight className="react-icon" />STEP 5 - Fly
+Book a flight, find accommodation & fly.
+(We organize a per-departure session)</li>
                   </ul>
                 </div>
-                <div className="">
-                  <h4 className="form-title">Apply For The Job</h4>
+               
+                  <h4 className="form-title">Apply For Appointment</h4>
                   <JobApplicationForm />
 
-                </div>
+               
               </div>
   
               <div className="summary-detail">
                 <div className="summary" >
-                  <h4 className="description-title">Job Summery</h4>
-                  <p><FaAngleRight className="react-icon" />Published On: 01 Jan, 2045</p>
-                  <p><FaAngleRight className="react-icon" />Vacancy: 123 Position</p>
-                  <p><FaAngleRight className="react-icon" />Job Nature: Full Time</p>
-                  <p><FaAngleRight className="react-icon" />Salary: $123 - $456</p>
-                  <p><FaAngleRight className="react-icon" />Location: New York, USA</p>
-                  <p><FaAngleRight className="react-icon" />Date Line: 01 Jan, 2045</p>
+                  <h4 className="description-title">OUR SPECIALTY</h4>
+                  <p><FaAngleRight className="react-icon" />Free assessment</p>
+                  <p><FaAngleRight className="react-icon" />Free application</p>
+                  <p><FaAngleRight className="react-icon" />No file opening & Services charge</p>
+                  <p><FaAngleRight className="react-icon" />Step-by-step Sponsor & Visa Guidelines</p>
+                  <p><FaAngleRight className="react-icon" />Expert & Dedicated Counselors</p>
+                  <p><FaAngleRight className="react-icon" />Assist in SOP</p>
+                  <p><FaAngleRight className="react-icon" />High Visa Success Rate</p>
+                  <p><FaAngleRight className="react-icon" />Funds assistance with a competitive market rate</p>
+                  <p><FaAngleRight className="react-icon" />Accommodation assistance including airport pick up</p>
                 </div>
                 <div className="detail" >
-                  <h4 className="description-title">Company Detail</h4>
-                  <p>Ipsum dolor ipsum accusam stet et et diam dolores, sed rebum sadipscing elitr vero dolores. Lorem dolore elitr justo et no gubergren sadipscing, ipsum et takimata aliquyam et rebum est ipsum lorem diam. Et lorem magna eirmod est et et sanctus et, kasd clita labore.</p>
+                  <h4 className="description-title">OUR STATEMENT</h4>
+                  <p>
+                  We are a trusted partner of prominent educational institutions in Australia & Canada
+We assist students genuinely to study High School Programs (Year 11 & Year 12), Vocational Studies, Bachelor Studies, Master's & Research Programs in Australia & Canada.
+                  </p>
                 </div>
               </div>
             </div>
